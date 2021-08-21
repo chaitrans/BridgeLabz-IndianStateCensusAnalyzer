740c7dae4cc54f87b7a9c4c704a29368e03dde3a
chaitra845 <chaitrachytra@gmail.com>
2021-08-21 19:08:18 +0530

    Reading Indian States Census Information from csv file and added some test cases

---
+++ IndianStateCensusAnalyzer/src/main/java/org/example/Exceptions/CustomizedExceptions.java
@@ -0,0 +1,10 @@
+package org.example.Exceptions;
+/**
+ * Purpose : To create customized Exception
+ */
+public class CustomizedExceptions extends Exception {
+
+    public CustomizedExceptions(String message) {
+        super(message);
+    }
+}
